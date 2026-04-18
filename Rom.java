d687c45441e09d3580dadac7a56805db29e30c26
Rom7699 <rom7699@gmail.com>
2026-04-18 12:42:12 +0300

    update to Rom.java

--- Rom.java
+++ Rom.java
@@ -1,7 +1,7 @@
 public class Rom {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Rom the king ..." + i);
+            System.out.println("Rom ..." + i);
         }
     }
 }
